7c0d3d206d88adeead0c47e518aba216ccdf536f
Paul Amazona <whatevergeek@gmail.com>
2020-12-29 01:32:10 +0800

    Add project files.

---
+++ JsonSchemaDemoLib/Validator.cs
@@ -0,0 +1,30 @@
+﻿using System;
+using System.Text.Json;
+
+using Json.Schema;
+
+
+namespace JsonSchemaDemoLib
+{
+    public class Validator
+    {
+        public string Schema { get; set; }
+        public bool IsValid(string data, out string result)
+        {
+            if (!string.IsNullOrWhiteSpace(Schema))
+            {
+                var doc = JsonDocument.Parse(data);
+                JsonSchema schema = JsonSchema.FromText(Schema);
+
+                var validationResult = schema.Validate(doc.RootElement);
+
+                result = validationResult.IsValid ? "Validation Succeeded." : validationResult.Message;
+                return validationResult.IsValid;
+            }
+            else
+            {
+                throw new Exception("Schema not set");
+            }
+        }
+    }
+}
---
+++ JsonSchemaDemoTest/ValidatorTest.cs
@@ -0,0 +1,38 @@
+﻿
+using System.Text;
+using JsonSchemaDemoLib;
+using Xunit;
+
+
+namespace JsonSchemaDemoTest
+{
+    public class ValidatorTest
+    {
+        [Fact]
+        public void TestValidCase()
+        {
+            var validator = new Validator
+            {
+                Schema = Encoding.UTF8.GetString(test_data.schema_sample1)
+            };
+
+            string message = string.Empty;
+            Assert.True(validator.IsValid(Encoding.UTF8.GetString(test_data.data_sample1),out message));
+            Assert.Equal("Validation Succeeded.", message);
+            
+        }
+
+        [Fact]
+        public void TestInvalidCase()
+        {
+            var validator = new Validator
+            {
+                Schema = Encoding.UTF8.GetString(test_data.schema_sample1)
+            };
+
+            string message = string.Empty;
+            Assert.True(!validator.IsValid(Encoding.UTF8.GetString(test_data.invalid_data_sample1), out message));
+            //Assert.Equal("Validation Succeeded.", message);
+        }
+    }
+}
