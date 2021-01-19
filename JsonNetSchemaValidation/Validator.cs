4524e6bbba8f3f82626836f7504be01f18c6439d
Paul Amazona <whatevergeek@gmail.com>
2021-01-19 18:17:57 +0800

    Added sample for for json.net handling of error message

---
+++ JsonNetSchemaValidation/Validator.cs
@@ -0,0 +1,42 @@
+﻿using System;
+using System.Collections.Generic;
+using Newtonsoft.Json.Linq;
+using Newtonsoft.Json.Schema;
+
+namespace JsonNetSchemaValidation
+{
+    public class Validator
+    {
+        public string Schema { get; set; }
+        public bool IsValid(string data, out string result)
+        {
+            if (!string.IsNullOrWhiteSpace(Schema))
+            {
+                var doc = JObject.Parse(data);
+                JsonSchema schema = JsonSchema.Parse(Schema);
+
+                IList<string> messages;
+                bool isValid = doc.IsValid(schema, out messages);
+
+                if (messages.Count > 0)
+                {
+                    result = "Validation Errors: ";
+                    foreach (var errorMessage in messages)
+                    {
+                        result += $"{errorMessage}| ";
+                    }
+                }
+                else
+                {
+                    result = "Validation Succeeded.";
+                }
+
+                return isValid;
+            }
+            else
+            {
+                throw new Exception("Schema not set");
+            }
+        }
+    }
+}
--- JsonSchemaDemoTest/ValidatorTest.cs
+++ JsonSchemaDemoTest/ValidatorTest.cs
@@ -1,5 +1,6 @@
 ﻿
 using System.Text;
+using JsonNetSchemaValidation;
 using JsonSchemaDemoLib;
 using Xunit;
 
@@ -9,9 +10,9 @@ namespace JsonSchemaDemoTest
     public class ValidatorTest
     {
         [Fact]
-        public void TestValidCase()
+        public void JsonSchemaTestValidCase()
         {
-            var validator = new Validator
+            var validator = new JsonSchemaDemoLib.Validator
             {
                 Schema = Encoding.UTF8.GetString(test_data.schema_sample1)
             };
@@ -23,16 +24,43 @@ namespace JsonSchemaDemoTest
         }
 
         [Fact]
-        public void TestInvalidCase()
+        public void JsonSchemaTestInvalidCase()
         {
-            var validator = new Validator
+            var validator = new JsonSchemaDemoLib.Validator
             {
                 Schema = Encoding.UTF8.GetString(test_data.schema_sample1)
             };
 
             string message = string.Empty;
             Assert.True(!validator.IsValid(Encoding.UTF8.GetString(test_data.invalid_data_sample1), out message));
-            //Assert.Equal("Validation Succeeded.", message);
+            Assert.Equal("Some error message at least", message);
+        }
+
+        [Fact]
+        public void JsonNetSchemaTestValidCase()
+        {
+            var validator = new JsonNetSchemaValidation.Validator
+            {
+                Schema = Encoding.UTF8.GetString(test_data.schema_sample1)
+            };
+
+            string message = string.Empty;
+            Assert.True(validator.IsValid(Encoding.UTF8.GetString(test_data.data_sample1), out message));
+            Assert.Equal("Validation Succeeded.", message);
+        }
+
+        [Fact]
+        public void JsonNetSchemaTestInvalidCase()
+        {
+            var validator = new JsonNetSchemaValidation.Validator
+            {
+                Schema = Encoding.UTF8.GetString(test_data.schema_sample1)
+            };
+
+            string message = string.Empty;
+            Assert.True(!validator.IsValid(Encoding.UTF8.GetString(test_data.invalid_data_sample1), out message));
+            var expectedError = "Validation Errors: Integer -1 is less than minimum value of 0. Line 4, position 12.| ";
+            Assert.Equal(expectedError, message);
         }
     }
 }
